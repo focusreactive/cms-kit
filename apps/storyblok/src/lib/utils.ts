dfe2ab98ed0d83e1af6973a73876388702b6a1e5
maksim hodasevich <maksim.hodasevich@gmail.com>
2025-01-10 15:30:41 +0100

    add common fetcher

--- apps/storyblok/src/lib/utils.ts
+++ apps/storyblok/src/lib/utils.ts
@@ -1,12 +1,26 @@
+import { SB_CACHE_VERSION_TAG } from "@/constants/cacheTags";
+
 export const isPreview = process.env.NEXT_PUBLIC_IS_PREVIEW === "true";
 
 export async function fetcher(slug: string, params: Record<string, unknown>) {
-  const now = performance.now();
-  const response = await fetch(slug, params);
-  const end = performance.now();
+  try {
+    const now = performance.now();
+    const response = await fetch(slug, params);
+    const end = performance.now();
 
-  console.log("fetcher execution time: ", (end - now).toFixed(2));
-  console.log(response.status, response.statusText);
+    console.log("fetcher execution time: ", (end - now).toFixed(2));
+    console.log(response.status, response.statusText);
 
-  return await response.json();
+    return await response.json();
+  } catch (error) {
+    console.error("Error in fetcher:", error);
+    throw error;
+  }
+}
+
+export function getNextCachingParams(version: "draft" | "published") {
+  return {
+    next: { tags: [SB_CACHE_VERSION_TAG] },
+    cache: version === "published" ? "default" : "no-store",
+  };
 }
