c9bda36c056d7e4109975815ef2582c495212fb5
maksim hodasevich <maksim.hodasevich@gmail.com>
2025-01-16 12:20:18 +0100

    Cleanup

--- apps/storyblok/CLI/utils/file.mjs
+++ apps/storyblok/CLI/utils/file.mjs
@@ -19,24 +19,14 @@ export function replaceTextInFile(path, oldText, newText) {
 }
 
 export function modifyJsonFile(path, transformer) {
-  fs.readFileSync(path, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
+  try {
+    const data = fs.readFileSync(path, "utf8");
     const content = JSON.parse(data);
     const modifiedContent = transformer(content);
 
-    fs.writeFileSync(
-      path,
-      JSON.stringify(modifiedContent, null, 2) + "\n",
-      (err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-      },
-    );
-  });
+    fs.writeFileSync(path, JSON.stringify(modifiedContent, null, 2) + "\n");
+  } catch (err) {
+    console.error(err);
+    return;
+  }
 }
