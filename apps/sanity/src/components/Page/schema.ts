469362cef4ac2d1a58d4b66f698a2ed2707ae5b8
maksim hodasevich <maksim.hodasevich@gmail.com>
2025-01-20 16:52:39 +0100

    add sectionsBody to content tab

--- apps/sanity/src/components/Page/schema.ts
+++ apps/sanity/src/components/Page/schema.ts
@@ -36,6 +36,7 @@ export default defineType({
       validation: (Rule) => Rule.required(),
     }),
     defineField({
+      group: CommonGroup.Content,
       name: "sectionsBody",
       type: "array",
       of: sections.map((section) => ({
