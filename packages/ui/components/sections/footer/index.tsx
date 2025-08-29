5803c377e3718140013d3d323510bd4b754774d1
maksim hodasevich <maksim.hodasevich@gmail.com>
2025-08-29 16:39:23 +0200

    refactor(footer): enhance Footer component layout and styling for improved responsiveness and visual consistency

--- packages/ui/components/sections/footer/index.tsx
+++ packages/ui/components/sections/footer/index.tsx
@@ -5,19 +5,24 @@ import type { IFooterProps } from "./types";
 
 export function Footer({ links, text, copywriteText, image }: IFooterProps) {
   return (
-    <footer className="space-y-12">
-      <div className="h-16">
-        <Image {...image} fit="contain" />
+    <footer className="pt- space-y-12">
+      <div className="flex flex-col items-center gap-x-12 gap-y-8 lg:flex-row">
+        <div className="flex flex-col items-start space-y-8">
+          <div className="h-24">
+            <Image {...image} fit="contain" />
+          </div>
+          <RichText {...text} />
+        </div>
+        <nav
+          className="flex grow flex-wrap items-center justify-end gap-3 p-3"
+          aria-label="footer mavigation"
+        >
+          {links.map((link, i) => (
+            <Link key={link.text + i} {...link} />
+          ))}
+        </nav>
       </div>
-      <RichText {...text} />
-      <nav
-        className="flex flex-wrap items-center justify-center gap-3 p-3"
-        aria-label="footer mavigation"
-      >
-        {links.map((link, i) => (
-          <Link key={link.text + i} {...link} />
-        ))}
-      </nav>
+
       {copywriteText && (
         <p className="text-textColor text-center">{copywriteText}</p>
       )}
