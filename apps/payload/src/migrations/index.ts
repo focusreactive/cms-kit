c13b087a1ec217718a3df94ed93a7f3989f15b7d
Pekarski <kiryl.pekarski.dev@gmail.com>
2026-04-10 13:19:49 +0300

    fix(payload): replace MCP migration with idempotent version

--- apps/payload/src/migrations/index.ts
+++ apps/payload/src/migrations/index.ts
@@ -1,27 +1,27 @@
-import * as migration_20260317_091401_init from './20260317_091401_init'
-import * as migration_20260317_092633_add_slug_unique_constraints from './20260317_092633_add_slug_unique_constraints'
-import * as migration_20260317_150738_create_ab_manifest__remove_page_variants from './20260317_150738_create_ab_manifest__remove_page_variants'
-import * as migration_20260317_181410_set_required_to_categories_slug from './20260317_181410_set_required_to_categories_slug'
-import * as migration_20260317_184349_forbid_localized_for_breadcrumbs from './20260317_184349_forbid_localized_for_breadcrumbs'
-import * as migration_20260317_195931_remove_localized_for_breadcrumbs from './20260317_195931_remove_localized_for_breadcrumbs'
-import * as migration_20260318_022640_create_comments from './20260318_022640_create_comments'
-import * as migration_20260318_140943_add_section_group_to_blocks from './20260318_140943_add_section_group_to_blocks'
-import * as migration_20260319_100637_add_image_fields from './20260319_100637_add_image_fields'
-import * as migration_20260319_103151_remove_section_fields_from_blog_section from './20260319_103151_remove_section_fields_from_blog_section'
-import * as migration_20260319_164752_update_header_and_footer_according_to_other_projects from './20260319_164752_update_header_and_footer_according_to_other_projects'
-import * as migration_20260325_115801_enhance_blog from './20260325_115801_enhance_blog'
-import * as migration_20260325_124045_return_blog_field from './20260325_124045_return_blog_field'
-import * as migration_20260325_124521_restructure_blog_field from './20260325_124521_restructure_blog_field'
-import * as migration_20260326_050306_remove_blog_section from './20260326_050306_remove_blog_section'
-import * as migration_20260326_051511_add_custom_page_type_to from './20260326_051511_add_custom_page_type_to'
-import * as migration_20260329_153412_add_presets_to_all_blocks from './20260329_153412_add_presets_to_all_blocks'
-import * as migration_20260401_110755_sync_migra from './20260401_110755_sync_migra'
-import * as migration_20260406_083656_create_document_embeddings from './20260406_083656_create_document_embeddings'
-import * as migration_20260406_122500_add_document_embeddings_unique_constraint from './20260406_122500_add_document_embeddings_unique_constraint'
-import * as migration_20260406_210202_drop_fts_content from './20260406_210202_drop_fts_content'
-import * as migration_20260406_233113_add_search_to_custom_page from './20260406_233113_add_search_to_custom_page'
-import * as migration_20260407_094009_slim_document_embeddings from './20260407_094009_slim_document_embeddings'
-import * as migration_20260408_155008_add_mcp from './20260408_155008_add_mcp'
+import * as migration_20260317_091401_init from './20260317_091401_init';
+import * as migration_20260317_092633_add_slug_unique_constraints from './20260317_092633_add_slug_unique_constraints';
+import * as migration_20260317_150738_create_ab_manifest__remove_page_variants from './20260317_150738_create_ab_manifest__remove_page_variants';
+import * as migration_20260317_181410_set_required_to_categories_slug from './20260317_181410_set_required_to_categories_slug';
+import * as migration_20260317_184349_forbid_localized_for_breadcrumbs from './20260317_184349_forbid_localized_for_breadcrumbs';
+import * as migration_20260317_195931_remove_localized_for_breadcrumbs from './20260317_195931_remove_localized_for_breadcrumbs';
+import * as migration_20260318_022640_create_comments from './20260318_022640_create_comments';
+import * as migration_20260318_140943_add_section_group_to_blocks from './20260318_140943_add_section_group_to_blocks';
+import * as migration_20260319_100637_add_image_fields from './20260319_100637_add_image_fields';
+import * as migration_20260319_103151_remove_section_fields_from_blog_section from './20260319_103151_remove_section_fields_from_blog_section';
+import * as migration_20260319_164752_update_header_and_footer_according_to_other_projects from './20260319_164752_update_header_and_footer_according_to_other_projects';
+import * as migration_20260325_115801_enhance_blog from './20260325_115801_enhance_blog';
+import * as migration_20260325_124045_return_blog_field from './20260325_124045_return_blog_field';
+import * as migration_20260325_124521_restructure_blog_field from './20260325_124521_restructure_blog_field';
+import * as migration_20260326_050306_remove_blog_section from './20260326_050306_remove_blog_section';
+import * as migration_20260326_051511_add_custom_page_type_to from './20260326_051511_add_custom_page_type_to';
+import * as migration_20260329_153412_add_presets_to_all_blocks from './20260329_153412_add_presets_to_all_blocks';
+import * as migration_20260401_110755_sync_migra from './20260401_110755_sync_migra';
+import * as migration_20260406_083656_create_document_embeddings from './20260406_083656_create_document_embeddings';
+import * as migration_20260406_122500_add_document_embeddings_unique_constraint from './20260406_122500_add_document_embeddings_unique_constraint';
+import * as migration_20260406_210202_drop_fts_content from './20260406_210202_drop_fts_content';
+import * as migration_20260406_233113_add_search_to_custom_page from './20260406_233113_add_search_to_custom_page';
+import * as migration_20260407_094009_slim_document_embeddings from './20260407_094009_slim_document_embeddings';
+import * as migration_20260410_095600_add_mcp from './20260410_095600_add_mcp';
 
 export const migrations = [
   {
@@ -140,8 +140,8 @@ export const migrations = [
     name: '20260407_094009_slim_document_embeddings',
   },
   {
-    up: migration_20260408_155008_add_mcp.up,
-    down: migration_20260408_155008_add_mcp.down,
-    name: '20260408_155008_add_mcp',
+    up: migration_20260410_095600_add_mcp.up,
+    down: migration_20260410_095600_add_mcp.down,
+    name: '20260410_095600_add_mcp'
   },
-]
+];
